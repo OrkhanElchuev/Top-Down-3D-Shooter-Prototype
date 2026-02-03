f125e29ac9349fac36d13bab21fb871c54e63c67
OrkhanElchuev <orkhan.elchuev@gmail.com>
2026-02-04 00:28:03 +0100

    Add System to Check which Item is the closest

--- Assets/Scripts/Pickups/Interactable.cs
+++ Assets/Scripts/Pickups/Interactable.cs
@@ -1,7 +1,34 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
+    [SerializeField] private MeshRenderer mesh;
+    [SerializeField] private Material highlightMaterial;
+    private Material defaultMaterial;
+
+    private void Start()
+    {
+        if (mesh == null)
+            mesh = GetComponentInChildren<MeshRenderer>();
+
+        defaultMaterial = mesh.material;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+            HighlightActive(true);
+    }
+
+    public void HighlightActive(bool active)
+    {
+        if (active)
+            mesh.material = highlightMaterial;
+        else
+            mesh.material = defaultMaterial;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         PlayerInteraction playerInteraction = other.GetComponent<PlayerInteraction>();
@@ -10,6 +37,7 @@ public class Interactable : MonoBehaviour
             return;
 
         playerInteraction.interactables.Add(this);
+        playerInteraction.UpdateClosestInteractable();
     }
 
     private void OnTriggerExit(Collider other)
@@ -20,5 +48,6 @@ public class Interactable : MonoBehaviour
             return;
 
         playerInteraction.interactables.Remove(this);
+        playerInteraction.UpdateClosestInteractable();
     }
 }
--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 public class PlayerInteraction : MonoBehaviour
 {
     public List<Interactable> interactables;
+
+    private Interactable closestInteracatable;
+
+    public void UpdateClosestInteractable()
+    {
+        closestInteracatable?.HighlightActive(false);
+        float closestDistance = float.MaxValue;
+
+        foreach (Interactable interactable in interactables)
+        {
+            float distance = Vector3.Distance(transform.position, interactable.transform.position);
+
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestInteracatable = interactable;
+            }
+        }
+
+        closestInteracatable?.HighlightActive(true);
+    }
 }
 

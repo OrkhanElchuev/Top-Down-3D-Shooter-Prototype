ebcc52bc28a40c7ff82ffc62e2e2e639a0656897
OrkhanElchuev <orkhan.elchuev@gmail.com>
2026-02-05 20:37:44 +0100

    Add Move State

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,25 +1,54 @@
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 public class Enemy : MonoBehaviour
 {
     [Header("Idle Settings")]
     public float idleTime;
-    
+
+    [Header("Move Settings")]
+    public float moveSpeed;
+    [SerializeField] private Transform[] patrolPoints;
+
+    private int currentPatrolIndex;
+
+    public NavMeshAgent agent { get; private set; }
     public EnemyStateMachine stateMachine { get; private set; }
 
     protected virtual void Awake()
     {
         stateMachine = new EnemyStateMachine();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     protected virtual void Start()
     {
-        
+        InitializePatrolPoints();
     }
 
     protected virtual void Update()
     {
         
+    }
+
+    public Vector3 GetPatrolDestination()
+    {
+        Vector3 destination = patrolPoints[currentPatrolIndex].transform.position;
+
+        currentPatrolIndex++;
+
+        if (currentPatrolIndex >= patrolPoints.Length)
+            currentPatrolIndex = 0;
+        
+        return destination;
+    }
+
+    private void InitializePatrolPoints()
+    {
+        foreach (Transform t in patrolPoints)
+        {
+            t.parent = null;
+        }
     }
 }
--- Assets/Scripts/Enemy/MoveStateMelee.cs
+++ Assets/Scripts/Enemy/MoveStateMelee.cs
@@ -2,24 +2,35 @@ using UnityEngine;
 
 public class MoveStateMelee : EnemyState
 {
+    private EnemyMelee enemy;
+    private Vector3 destination;
+
     public MoveStateMelee(Enemy enemyBase, EnemyStateMachine stateMachine, string animationBoolName) : base(enemyBase, stateMachine, animationBoolName)
     {
+        enemy = enemyBase as EnemyMelee;
     }
 
     public override void Enter()
     {
         base.Enter();
+
+        destination = enemy.GetPatrolDestination();
     }
 
     public override void Exit()
     {
         base.Exit();
+
+        Debug.Log("Exit Move State");
     }
 
     public override void Update()
     {
         base.Update();
 
-        Debug.Log("I m moving");
+        enemy.agent.SetDestination(destination);
+
+        if (enemy.agent.remainingDistance <= 1)
+            stateMachine.ChangeState(enemy.idleState);
     }
 }

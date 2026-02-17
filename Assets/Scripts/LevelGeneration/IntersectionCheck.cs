79139028f98bbf76f339ebd8e35775edfc4e79d8
OrkhanElchuev <orkhan.elchuev@gmail.com>
2026-02-17 18:15:31 +0100

    Add Tooltips, regions, documentation and description to Level Generation Scripts

--- Assets/Scripts/LevelGeneration/IntersectionCheck.cs
+++ Assets/Scripts/LevelGeneration/IntersectionCheck.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
 
+/// <summary>
+/// Marker component used by <see cref="LevelPart.IntersectionDetected"/> to determine
+/// whether an overlap hit belongs to a level part's intersection-check group.
+/// 
+/// Put this on the root transform (or a known parent) of the "intersection check" collider set.
+/// </summary>
+
 public class IntersectionCheck : MonoBehaviour
 {
-    
+    // Intentionally empty (marker component).
 }
